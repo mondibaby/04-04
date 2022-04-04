67c0ac30f4222504426a9ac0a626760d24400d63
mondibaby <101931498+mondibaby@users.noreply.github.com>
2022-04-04 17:12:32 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,17 @@
+class mobile{
+    constructor (color,name){
+        this.color=color;
+        this.name=name;
+    }
+    calling(){
+        console.log('call to chinni');
+    }
+    playpubg(){
+        console.log('intro-music->shoot eney->');
+
+    }
+    takepictre(){
+        console.log('smile please->. nice,kee smiling');
+    }
+}
+var apple=new amobile ('')
---
+++ function.js
@@ -0,0 +1,48 @@
+// function add(a,b){
+//     var c=a+b;
+//     console.log(c);
+// }
+// function sub(a,b){
+//     var c=a-b;
+//     console.log(c);
+// }
+// function mul(){
+//     var a=30;
+//     var b=40;
+//     var c=a*b;
+//     return c;
+// }
+// var a=30;
+// var b=40;
+// var c=add(a,b);
+// var c=sub(a,b)
+// console.log(c);
+// var d=mul(a,b)
+//  console.log(d)
+
+
+
+function add(a,b){
+    var a,b,c;
+    c=a+b;
+    return c;
+}
+var c=add(10,20);
+console.log(c);
+
+
+function sub(a,b){
+    var a,b,x
+    x=a-b;
+    return x;
+}
+var x=sub(10,30);
+console.log(x);
+
+function mul(a,b){
+    var a,b,z;
+    z=a*b;
+    return z;
+}
+var z=mul(20,30);
+console.log(z);
